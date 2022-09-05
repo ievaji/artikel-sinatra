578d9a6bb884fb085f88096f5f70bc9ada1380a1
ieva jirgensone <ieva.ji.mail@gmail.com>
2022-09-05 10:02:54 +0200

    Bugfixes and some refactoring

--- classes/cleaner.rb
+++ classes/cleaner.rb
@@ -1,22 +1,38 @@
 # frozen_string_literal: true
 
-# Cleaner is a helper class that cleans data for both Word and Finder.
+# Cleaner cleans data for both Word and Finder.
 class Cleaner
   def initialize; end
 
-  # for Word
-  def clean(word)
-    word.delete(' ').gsub(/\P{L}/, 'x').downcase
+  # Word : initialize : clean the value assigned to it
+  def clean(str)
+    str.delete(' ').gsub(/\P{L}/, 'x').downcase
   end
 
-  # for Finder
-  def prepare(data)
-    arr = data.split('Übersetzungen')
+  # Finder : exclude_names : clean the first element of a dataset
+  def prepare(str)
+    arr = str.split('Übersetzungen')
     cleaned = arr.shift.split(' (Deutsch)').pop
     arr.unshift(cleaned)
   end
 
-  # for Finder
+  # Finder : various methods : getting elements and their text
+  def headline_em(response)
+    response.search('#mw-content-text h3 .mw-headline em')
+  end
+
+  def headline_text(response)
+    response.search('#mw-content-text h3 .mw-headline').text.strip
+  end
+
+  def toc_element_text(response)
+    response.search('#toc .toctext').text.strip
+  end
+
+  def table_text(response)
+    response.search('#mw-content-text .mw-parser-output table').text.strip
+  end
+
   def clean_table_text(str)
     arr = str.split
     arr.length > 7 ? arr.last(7).join(' ') : str
--- classes/finder.rb
+++ classes/finder.rb
@@ -10,8 +10,8 @@ class Finder
 
   def initialize(word)
     @word = convert_to_unicode(word)
-    @cleaner = Cleaner.new
     @results = []
+    @cleaner = Cleaner.new
   end
 
   def find_artikel
@@ -19,6 +19,9 @@ class Finder
     return results if response.include?('Not found')
 
     return results if regional_spelling?(response)
+
+    # only_plural? must come before declinated_form? to filter correctly
+    return results << 'Plural' if only_plural?(response)
 
     return results if declinated_form?(response)
 
@@ -42,46 +45,74 @@ class Finder
     'ß' => '%C3%9F'
   }.freeze
 
+  # Method sets listed from last to first based on find_artikel flow
+  #
+  # 5. Extracting Artikel from pre-filtered data
   def extract_artikel(response)
-    return unless exclude_last_names(response).is_a?(Nokogiri::HTML4::Document)
+    return unless exclude_names(response).is_a?(Nokogiri::HTML4::Document)
 
-    response.search('#mw-content-text h3 .mw-headline em').each do |element|
+    @cleaner.headline_em(response).each do |element|
       results << ARTIKEL[element.text]
     end
   end
 
-  def exclude_last_names(response)
-    data = response.search('#toc .toctext').text.strip
-    return response unless data.include?('Nachname')
+  def exclude_names(response)
+    dataset_one = @cleaner.toc_element_text(response)
+    dataset_two = @cleaner.headline_text(response)
 
-    cleaned = @cleaner.prepare(data)
+    case_one = includes_names?(dataset_one)
+    case_two = includes_names?(dataset_two)
+
+    return response unless case_one || case_two
+
+    case_one ? process(dataset_one) : results
+  end
+
+  def process(dataset)
+    cleaned = @cleaner.prepare(dataset)
     cleaned.each do |unit|
-      unless unit.include?('Nachname') || unit.include?('nym')
+      unless includes_names?(unit)
         unit.split(', ').each { |item| results << ARTIKEL[item] if ARTIKEL.key?(item) }
       end
     end
   end
 
+  def includes_names?(data)
+    data.include?('name') || data.include?('nym')
+  end
+
+  # 4. Checking for regional spelling
   def regional_spelling?(response)
-    text = extract_table_text(response)
+    text = @cleaner.table_text(response)
     return false unless text.include?('andere Schreibung')
 
     results << text.split('.').first
     true
   end
 
+  # 3. Checking if it's a declinated form
   def declinated_form?(response)
-    text = extract_table_text(response)
+    text = @cleaner.table_text(response)
     return false unless text.include?('flektierte Form') && text.length < 400
 
     results << @cleaner.clean_table_text(text.split('.').first)
     true
   end
 
-  def extract_table_text(response)
-    response.search('#mw-content-text .mw-parser-output table').text.strip
+  # 2. Checking if it's a plural noun
+  def only_plural?(response)
+    no_genus_info?(response) && a_noun?(response)
   end
 
+  def no_genus_info?(response)
+    @cleaner.headline_em(response).empty?
+  end
+
+  def a_noun?(response)
+    @cleaner.headline_text(response).include?('Substantiv')
+  end
+
+  # 1. Getting a response and processing it, if not 404
   def net_response(word)
     response = Net::HTTP.get_response(URI("#{URL}#{word}"))
     if response.code == '404'
@@ -92,6 +123,7 @@ class Finder
     end
   end
 
+  # 0. Converting the word to Unicode (German special chars only)
   def convert_to_unicode(word)
     word.value.chars.map! { |char| UNICODE[char] || char }.join
   end
